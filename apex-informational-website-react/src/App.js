d3a9426c4a87b3f07cb2dcef4387af39a33fa249
kibaara-wangu <kibaarawangu@gmail.com>
2024-07-16 14:16:00 +0300

    added media query

--- apex-informational-website-react/src/App.js
+++ apex-informational-website-react/src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 // import './App.css';
-
+import Footer from './Footer';
 import About from './AboutUs';
 function App() {
   return (
     <div>
      <About/>
+     <Footer/>
+     
     </div>
   );
 }
